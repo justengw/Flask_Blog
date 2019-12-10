73f0452356c4fa7e543a5b4c97ae70f702158bf4
justengw <justengwatkins@gmail.com>
2019-12-10 07:00:00 -0800

    modified:   flaskblog.py 	modified:   templates/layout.html 	modified:   templates/login.html 	modified:   templates/register.html

--- flaskblog.py
+++ flaskblog.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '5898913c333d13b9b39519ecedcb1c1a'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
+db = SQLAlchemy(app)
 
 posts = [
     {
@@ -37,9 +40,15 @@ def register():
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful. Please check username and password.', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
